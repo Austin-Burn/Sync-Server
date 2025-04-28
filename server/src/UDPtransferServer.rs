b4bffde07b77deaa179b683e339c63c753d152b1
Austin Blackburn <austinburn79999999@gmail.com>
2025-04-28 14:44:46 -0400

    structure starting to show more, incomplete pushed for transfer

--- server/src/UDPtransferServer.rs
+++ server/src/UDPtransferServer.rs
@@ -4,12 +4,19 @@ use std::collections::HashMap;
 use dashmap::DashMap;
 use threadpool::ThreadPool;
   
+pub fn init_server(type: bool, ) -> io::Result<()> {
+
+}
+
 
 fn spawn_delegator_thread() -> io::Result<()> {
     make_new_thread()
 }
 
+
+
 pub struct UDPServer {
+    task_max_backlog: u8,
     chunksize: u64,
     packet_size: u32,
     target_ip: Option<String>,
@@ -17,17 +24,42 @@ pub struct UDPServer {
     upload_download: bool,
     active_ports: Vec<u16>,
     //1 is data, 2 is add thread, 3 is remove thread, 4 is end connection
-    commands: HashMap<String, fn() -> io::Result<()>>,
-    packets: DashMap<String, Vec<u8>>,
+    commands: HashMap<String, Box<dyn Fn() -> io::Result<()> + Send + Sync>>,
     worker_threads: DashMap<String, Thread>,
     delegator_thread: Thread,
-    buffer_one: Vec<u8>,
-    buffer_two: Vec<u8>,
-    buffer_three: Vec<u8>,
-    
+    data_buffer_one: Vec<u8>,
+    data_buffer_two: Vec<u8>,
+    data_buffer_three: Vec<u8>,
+    thread_queues: DashMap<String, Vec<u8>>,
+}
+
+pub struct load_buffer {
+    const type: u8 = 1,
+    data_pointer: u8,
+    buff_size: u8,
 }
 
 impl UDPServer {
+
+    fn spawn_worker_thread() -> io::Result<()> {
+        make_new_thread()
+        struct thread_queue {
+            thread_id: String,
+            buffer_switch: bool,
+            thread_que_one: Vec<u8>,
+            thread_que_two: Vec<u8>,
+
+        }
+        
+        self.thread_queues.insert(thread_id, thread_queue {
+            thread_id: thread_id,
+            buffer_switch: false,
+            thread_que_one: Vec::new(),
+            thread_que_two: Vec::new(),
+        }   );
+    
+    }
+
     pub fn new(addr: &str) -> io::Result<Self> {
         let socket = UdpSocket::bind(addr)?;
         Ok(Self { socket, upload_download: false, active_ports: Vec::new(), commands: HashMap::new(), packets: DashMap::new() })
@@ -64,20 +96,9 @@ impl UDPServer {
         make_new_thread()
     }
     
-    fn spawn_thread_worker() -> io::Result<()> {
-        make_new_thread()
-        while true {
-            let packet = buffer_one.pop_front();
-            if packet.is_some() {
-                let packet = packet.unwrap();
-                let packet_data = packet.data;
-            }
-        }
-    }   
     
-    fn spawn_delegator_thread() -> io::Result<()> {
-        make_new_thread()
-    }
+    
+
 
     pub fn run(&self) -> io::Result<()> {
         let mut buf = [0; 1024];
